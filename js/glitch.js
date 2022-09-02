8fb83cbc9ba1cd58b850141898d0ce951466fd23
Hunter Wheat <elitewheatproductionz@gmail.com>
2022-09-01 22:40:34 -0700

    adding glitch script

---
+++ js/glitch.js
@@ -0,0 +1,59 @@
+const Direction = {
+    'a': 'alpha',
+    'b': 'beta',
+    'c': '',
+    'd': ['eth', 'dum', 'delta'],
+    'e': 'schwa',
+    'f': '',
+    'g': ['gamma', 'gha'],
+    'h': ['heng', 'hwair'],
+    'i': 'iota',
+    'j': '',
+    'k': 'kra',
+    'l': 'lum',
+    'm': 'mum',
+    'n': ['num', 'eng'],
+    'o': ['closed omega', 'ou'],
+    'p': 'eth',
+    'q': 'eth',
+    'r': 'eth',
+    's': 'eth',
+    't': 'eth',
+    'u': 'eth',
+    
+    'v': 'eth',
+    
+    'w': 'eth',
+    
+    'x': 'chi',
+    'y': 'eth',
+    'z': 'eth',
+
+    
+  }
+
+  const samplingblocks = 
+  [
+
+  ]
+const Diacriticsbelow = []
+
+
+  function glitch_generate(text, percent, limit)
+  {
+    //percent = percentage that each character will be fully zalgo-ified
+    //max alterations to character
+
+    let text_buffer = text
+    maska = 0 // latin extensions
+    maskb = 0 // diacritics above
+    maskc = 0 //diacritics below
+    //generate mask
+    for (let i = 0; i < text.length; i++)
+    {
+      for (let j = 0; j < limit; j++)
+      {
+          
+      }
+    }
+  };
